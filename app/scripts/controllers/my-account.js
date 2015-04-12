fe673992d1fce1b7d5ab373e6b5f518cf1f858ce
AJ ONeal <awesome@coolaj86.com>
2015-04-12 02:43:44 -0600

    began API explorer

--- app/scripts/controllers/my-account.js
+++ app/scripts/controllers/my-account.js
@@ -2,35 +2,62 @@
 
 angular.module('yololiumApp')
   .controller('MyAccountController', [
-    '$scope'
+    '$window'
+  , '$scope'
   , '$location'
   , '$http'
   , 'LdsApiConfig'
   , 'LdsApiSession'
   , 'LdsApiRequest'
-  , function ($scope, $location, $http, LdsApiConfig, LdsApiSession, LdsApiRequest) {
+  , function ($window, $scope, $location, $http, LdsApiConfig, LdsApiSession, LdsApiRequest) {
     var scope = this;
 
+    function update() {
+      // can't pass anything to ng-options via ng-change, hence using scope
+      console.log('updateAccount', scope.selectedAccount);
+      if ('new' === scope.selectedAccount.appScopedId || scope.selectedAccount.new) {
+        scope.showLoginModal();
+        return;
+      }
+    }
+
+    scope.profiles = [];
+    scope.updateAccount = update;
+
     function init(session) {
+      // if it has a message, it's an error
       if (!session || session.message) {
         scope.session = null;
         scope.account = null;
-        scope.accounts = null;
+        scope.accounts = [];
+        scope.profiles = [];
         $location.url('/');
         return;
       }
 
       scope.session = session;
-      scope.accounts = session.accounts;
-      scope.account = LdsApiSession.account(session);
+      //scope.accounts = session.accounts;
+      scope.account = LdsApiSession.selectAccount(session);
 
-      console.log('session', session);
-      return LdsApiRequest.profile(session);
+      scope.accounts = [];
+      return LdsApiRequest.getAccountSummaries(session, scope.accounts).then(function () {
+        scope.accounts.forEach(function (account) {
+          console.log('account', account);
+          account.profile.me.photo = LdsApiRequest.photoUrl(account, account.profile.me.photos[0], 'medium');
+        });
+        console.log('session', session);
+        console.log('scope.accounts', scope.accounts);
+        //console.log('profiles', scope.profiles);
+        //return LdsApiRequest.profile(session);
+      });
     }
 
+    scope.switchUser = function () {
+      scope.showLoginModal();
+    };
     scope.showLoginModal = function () {
       // TODO profile manager
-      return LdsApiSession.login();
+      return LdsApiSession.login({ force: true, scope: '!' });
       // LdsApiSession.logins.implicitGrant();
       // LdsApiSession.logins.AuthorizationCode();
       // LdsApiSession.openAuthorizationDialog();
